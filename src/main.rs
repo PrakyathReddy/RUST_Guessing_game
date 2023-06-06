844a1bc9e01b558916735761f26d839224d55d71
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-06 16:30:14 +0530

    added rand library from crates.io by updating cargo.toml file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![allow(non_snake_case)]
 
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
     println!("Please input your guess");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);  
 
     let mut guess = String::new();
     
@@ -12,6 +15,8 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("you guessed: {guess}");5
+    println!("you guessed: {guess}");
+
+    println!("The secret number is {secret_number}");
 
 }
