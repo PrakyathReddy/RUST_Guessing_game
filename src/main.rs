30ea737146b64cdd7a2115b7ae24b36eb726d199
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-06 18:14:52 +0530

    added loop to continue asking and iterating until user wins

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,28 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number");
-    println!("Please input your guess");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);  
-
-    let mut guess = String::new();
     
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!!");
-
-    println!("you guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You Win!!"),
-    }
-
     println!("The secret number is {secret_number}");
+
+    loop {
+        println!("Please input your guess");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = guess.trim().parse().expect("Please type a number!!");
+
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => println!("You Win!!"),
+        }
+    }
 
 }
