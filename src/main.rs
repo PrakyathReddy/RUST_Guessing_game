e567a2a12fc8b76df3bb0d698cdccb5f5e135a52
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-08 14:06:04 +0530

    added some comments sample

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
             Ordering::Equal => {
                 println!("You Win!!");
                 break;
-            },
+            }, //adding some samople comment
         }
     }
 
